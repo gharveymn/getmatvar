6afd6aef5cc7035ffdbfaf1ef7bfea31f533ab08
courtneyum <bonnerc3@myumanitoba.ca>
2017-07-10 14:59:22 -0500

    Easy file navigation and dual maps
    mapping and unmapping is now transparent to the programmer with the navigateTo() function.

--- v7.3Files/mapping.c
+++ v7.3Files/mapping.c
@@ -9,17 +9,20 @@
 #include <errno.h>
 #include <stdint.h>
 #include <math.h>
+#include <assert.h>
 
-#define TRUE 0
-#define FALSE 1
-#define CHUNK 512
+#define TRUE 1
+#define FALSE 0
 #define FORMAT_SIG "\211HDF\r\n\032\n"
 #define MAX_Q_LENGTH 20
+#define TREE 0
+#define HEAP 1
+#define UNDEF_ADDR 0xffffffffffffffff
 
 typedef struct
 {
-	long tree_address;
-	long heap_address;
+	uint64_t tree_address;
+	uint64_t heap_address;
 } Addr_Pair;
 
 typedef struct
@@ -39,21 +42,36 @@ typedef struct
 	uint64_t base_address;
 } Superblock;
 
+typedef struct 
+{
+	char* map_start;
+	uint64_t bytes_mapped;
+	uint64_t offset;
+	int used;
+} MemMap;
+
 char* findSuperblock(int fd, size_t file_size);
 long getAddress(char* chunk_start, int num_bytes);
 void enqueuePair(Addr_Pair pair);
 Addr_Pair dequeuePair();
+Superblock fillSuperblock(char* chunk_start);
+char* navigateTo(uint64_t address, int map_index);
 
-size_t bytes_mapped;
-char* map_start;
+MemMap maps[2];
 Addr_Q queue;
+int fd;
 
 int main (int argc, char* argv[])
 {
 	char* filename = "my_struct.mat";
 	char* chunk_start;
-	bytes_mapped = 0;
 	Superblock s_block;
+	char* tree_pointer;
+	char* heap_pointer;
+
+	//init maps
+	maps[0].used = FALSE;
+	maps[1].used = FALSE;
 
 	//init queue
 	queue.front = 0;
@@ -61,7 +79,7 @@ int main (int argc, char* argv[])
 	queue.back = 0;
 
 	//open the file descriptor
-	int fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDWR);
 	if (fd < 0)
 	{
 		printf("open() unsuccessful, Check errno: %d\n", errno);
@@ -73,26 +91,25 @@ int main (int argc, char* argv[])
 
 	//find superblock
 	chunk_start = findSuperblock(fd, file_size);
-	
-	//get stuff from superblock, for now assume consistent versions of stuff
-	s_block.size_of_offsets = getAddress(chunk_start + 13, 1);
-	s_block.size_of_lengths = getAddress(chunk_start + 14, 1);
-	s_block.leaf_node_k = getAddress(chunk_start + 16, 2);
-	s_block.internal_node_k = getAddress(chunk_start + 18, 2);
-	s_block.base_address = getAddress(chunk_start + 24, 8);
+	s_block = fillSuperblock(chunk_start);
 
-	//read scratchpad space
-	char* sps_start = chunk_start + 80;
-	Addr_Pair root_pair;
-	root_pair.tree_address = getAddress(sps_start, 8) + s_block.base_address;
-	root_pair.heap_address = getAddress(sps_start + 8, 8) + s_block.base_address;
-	enqueuePair(root_pair);
-
-	if (munmap(map_start, bytes_mapped) != 0)
+	//unmap superblock
+	if (munmap(maps[0].map_start, maps[0].bytes_mapped) != 0)
 	{
 		printf("munmap() unsuccessful, Check errno: %d\n", errno);
 		exit(EXIT_FAILURE);
 	}
+	maps[0].used = FALSE;
+
+	while (queue.length > 0)
+	{
+		tree_pointer = navigateTo(queue.pairs[queue.front].tree_address, TREE);
+		assert(strncmp("TREE", tree_pointer, 4) == 0);
+		heap_pointer = navigateTo(queue.pairs[queue.front].heap_address, HEAP);
+		assert(strncmp("HEAP", heap_pointer, 4) == 0);
+		dequeuePair();
+	}
+	
 	exit(EXIT_SUCCESS);
 }
 
@@ -102,22 +119,25 @@ char* findSuperblock(int fd, size_t file_size)
 	size_t page_size = sysconf(_SC_PAGE_SIZE);
 
 	//Assuming that superblock is in first 8 512 byte chunks
-	map_start = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-	bytes_mapped = page_size;
-	if (map_start == NULL || map_start == MAP_FAILED)
+	maps[0].map_start = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	maps[0].bytes_mapped = page_size;
+	maps[0].offset = 0;
+	maps[0].used = TRUE;
+
+	if (maps[0].map_start == NULL || maps[0].map_start == MAP_FAILED)
 	{
 		printf("mmap() unsuccessful, Check errno: %d\n", errno);
 		exit(EXIT_FAILURE);
 	}
 
-	chunk_start = map_start;
+	chunk_start = maps[0].map_start;
 	
-	while (strncmp(FORMAT_SIG, chunk_start, 8) != 0 && (chunk_start - map_start) < page_size)
+	while (strncmp(FORMAT_SIG, chunk_start, 8) != 0 && (chunk_start - maps[0].map_start) < page_size)
 	{
 		chunk_start += 512;
 	}
 
-	if ((chunk_start - map_start) >= page_size)
+	if ((chunk_start - maps[0].map_start) >= page_size)
 	{
 		printf("Couldn't find superblock in first 8 512-byte chunks. I am quitting.\n");
 		exit(EXIT_FAILURE);
@@ -171,4 +191,46 @@ Addr_Pair dequeuePair()
 	}
 	queue.length--;
 	return pair;
+}
+Superblock fillSuperblock(char* chunk_start)
+{
+	Superblock s_block;
+	//get stuff from superblock, for now assume consistent versions of stuff
+	s_block.size_of_offsets = getAddress(chunk_start + 13, 1);
+	s_block.size_of_lengths = getAddress(chunk_start + 14, 1);
+	s_block.leaf_node_k = getAddress(chunk_start + 16, 2);
+	s_block.internal_node_k = getAddress(chunk_start + 18, 2);
+	s_block.base_address = getAddress(chunk_start + 24, 8);
+
+	//read scratchpad space
+	char* sps_start = chunk_start + 80;
+	Addr_Pair root_pair;
+	root_pair.tree_address = getAddress(sps_start, 8) + s_block.base_address;
+	root_pair.heap_address = getAddress(sps_start + 8, 8) + s_block.base_address;
+	enqueuePair(root_pair);
+
+	return s_block;
+}
+char* navigateTo(uint64_t address, int map_index)
+{
+	//ensure there are always 1024 bytes available for reading if we don't remap
+	if (maps[map_index].used && address >= maps[map_index].offset && address < maps[map_index].bytes_mapped - 1024)
+	{
+		return maps[map_index].map_start + address;
+	}
+	else
+	{
+		size_t page_size = sysconf(_SC_PAGE_SIZE);
+		maps[map_index].offset = (off_t)(address/page_size);
+		maps[map_index].map_start = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, maps[map_index].offset);
+		maps[map_index].bytes_mapped = page_size;
+		maps[map_index].used = TRUE;
+		if (maps[map_index].map_start == NULL || maps[map_index].map_start == MAP_FAILED)
+		{
+			printf("mmap() unsuccessful, Check errno: %d\n", errno);
+			exit(EXIT_FAILURE);
+		}
+		return maps[map_index].map_start + (address % page_size);
+	}
+	
 }
