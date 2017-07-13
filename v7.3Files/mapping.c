0aa080c1e4d6d329b2a1f0b9a472847d962bca9a
courtneyum <bonnerc3@myumanitoba.ca>
2017-07-13 15:18:32 -0500

    Find header for specified object
    Does not yet work for struct fields.

--- v7.3Files/mapping.c
+++ v7.3Files/mapping.c
@@ -18,6 +18,7 @@
 #define TREE 0
 #define HEAP 1
 #define UNDEF_ADDR 0xffffffffffffffff
+#define SYM_TABLE_ENTRY_SIZE 40
 
 typedef struct
 {
@@ -50,24 +51,35 @@ typedef struct
 	int used;
 } MemMap;
 
+typedef struct
+{
+	uint64_t name_offset;
+	uint64_t obj_header_address;
+} Object;
+
+Superblock getSuperblock(int fd, size_t file_size);
 char* findSuperblock(int fd, size_t file_size);
-long getAddress(char* chunk_start, int num_bytes);
+uint64_t getBytesAsNumber(char* chunk_start, int num_bytes);
 void enqueuePair(Addr_Pair pair);
 Addr_Pair dequeuePair();
-Superblock fillSuperblock(char* chunk_start);
+Superblock fillSuperblock(char* superblock_pointer);
 char* navigateTo(uint64_t address, int map_index);
+void readTreeNode(char* tree_address);
+uint64_t readSnod(char* snod_pointer, char* heap_pointer, char* var_name);
 
 MemMap maps[2];
 Addr_Q queue;
 int fd;
+Superblock s_block;
 
 int main (int argc, char* argv[])
 {
 	char* filename = "my_struct.mat";
-	char* chunk_start;
-	Superblock s_block;
+	char* variable_name = "my_struct.array";
 	char* tree_pointer;
 	char* heap_pointer;
+
+	uint64_t header_address = 0;
 
 	//init maps
 	maps[0].used = FALSE;
@@ -90,29 +102,45 @@ int main (int argc, char* argv[])
 	size_t file_size = lseek(fd, 0, SEEK_END);
 
 	//find superblock
-	chunk_start = findSuperblock(fd, file_size);
-	s_block = fillSuperblock(chunk_start);
-
-	//unmap superblock
-	if (munmap(maps[0].map_start, maps[0].bytes_mapped) != 0)
-	{
-		printf("munmap() unsuccessful, Check errno: %d\n", errno);
-		exit(EXIT_FAILURE);
-	}
-	maps[0].used = FALSE;
+	s_block = getSuperblock(fd, file_size);
 
 	while (queue.length > 0)
 	{
 		tree_pointer = navigateTo(queue.pairs[queue.front].tree_address, TREE);
-		assert(strncmp("TREE", tree_pointer, 4) == 0);
 		heap_pointer = navigateTo(queue.pairs[queue.front].heap_address, HEAP);
 		assert(strncmp("HEAP", heap_pointer, 4) == 0);
+
+		if (strncmp("TREE", tree_pointer, 4) == 0)
+		{
+			readTreeNode(tree_pointer);
+		}
+		else if (strncmp("SNOD", tree_pointer, 4) == 0)
+		{
+			header_address = readSnod(tree_pointer, heap_pointer, variable_name);
+			break;
+		}
+
 		dequeuePair();
 	}
+	printf("Object header for variable %s is at 0x%lx\n", variable_name, header_address);
 	
 	exit(EXIT_SUCCESS);
 }
+Superblock getSuperblock(int fd, size_t file_size)
+{
+	char* superblock_pointer = findSuperblock(fd, file_size);
+	Superblock s_block = fillSuperblock(superblock_pointer);
 
+	//unmap superblock
+	if (munmap(maps[0].map_start, maps[0].bytes_mapped) != 0)
+	{
+		printf("munmap() unsuccessful in getSuperblock(), Check errno: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+	maps[0].used = FALSE;
+
+	return s_block;
+}
 char* findSuperblock(int fd, size_t file_size)
 {
 	char* chunk_start = "";
@@ -145,13 +173,18 @@ char* findSuperblock(int fd, size_t file_size)
 
 	return chunk_start;
 }
-long getAddress(char* chunk_start, int num_bytes)
+uint64_t getBytesAsNumber(char* chunk_start, int num_bytes)
 {
-	long ret = 0;
+	uint64_t ret = 0;
 	int n = 0;
+	uint8_t byte = 0;
+	uint64_t temp = 0;
 	while (n < num_bytes)
 	{
-		ret += (*(chunk_start + n) << (8*n)) & (long)(pow(2, 8*n + 8) - 1);
+		byte = *(chunk_start + n);
+		temp = byte;
+		temp = temp << (8*n);
+		ret += temp;
 		n++;
 	}
 	return ret;
@@ -192,21 +225,21 @@ Addr_Pair dequeuePair()
 	queue.length--;
 	return pair;
 }
-Superblock fillSuperblock(char* chunk_start)
+Superblock fillSuperblock(char* superblock_pointer)
 {
 	Superblock s_block;
 	//get stuff from superblock, for now assume consistent versions of stuff
-	s_block.size_of_offsets = getAddress(chunk_start + 13, 1);
-	s_block.size_of_lengths = getAddress(chunk_start + 14, 1);
-	s_block.leaf_node_k = getAddress(chunk_start + 16, 2);
-	s_block.internal_node_k = getAddress(chunk_start + 18, 2);
-	s_block.base_address = getAddress(chunk_start + 24, 8);
+	s_block.size_of_offsets = getBytesAsNumber(superblock_pointer + 13, 1);
+	s_block.size_of_lengths = getBytesAsNumber(superblock_pointer + 14, 1);
+	s_block.leaf_node_k = getBytesAsNumber(superblock_pointer + 16, 2);
+	s_block.internal_node_k = getBytesAsNumber(superblock_pointer + 18, 2);
+	s_block.base_address = getBytesAsNumber(superblock_pointer + 24, s_block.size_of_offsets);
 
 	//read scratchpad space
-	char* sps_start = chunk_start + 80;
+	char* sps_start = superblock_pointer + 80;
 	Addr_Pair root_pair;
-	root_pair.tree_address = getAddress(sps_start, 8) + s_block.base_address;
-	root_pair.heap_address = getAddress(sps_start + 8, 8) + s_block.base_address;
+	root_pair.tree_address = getBytesAsNumber(sps_start, s_block.size_of_offsets) + s_block.base_address;
+	root_pair.heap_address = getBytesAsNumber(sps_start + s_block.size_of_offsets, s_block.size_of_offsets) + s_block.base_address;
 	enqueuePair(root_pair);
 
 	return s_block;
@@ -220,6 +253,19 @@ char* navigateTo(uint64_t address, int map_index)
 	}
 	else
 	{
+		//unmap current page if used
+		if (maps[map_index].used)
+		{
+			if (munmap(maps[map_index].map_start, maps[map_index].bytes_mapped) != 0)
+			{
+				printf("munmap() unsuccessful in navigateTo(), Check errno: %d\n", errno);
+				printf("1st arg: %s\n2nd arg: %lu\nUsed: %d\n", maps[map_index].map_start, maps[map_index].bytes_mapped, maps[map_index].used);
+				exit(EXIT_FAILURE);
+			}
+			maps[map_index].used = FALSE;
+		}
+
+		//map new page at needed location
 		size_t page_size = sysconf(_SC_PAGE_SIZE);
 		maps[map_index].offset = (off_t)(address/page_size);
 		maps[map_index].map_start = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, maps[map_index].offset);
@@ -233,4 +279,74 @@ char* navigateTo(uint64_t address, int map_index)
 		return maps[map_index].map_start + (address % page_size);
 	}
 	
+}
+void readTreeNode(char* tree_pointer)
+{
+	Addr_Pair pair;
+	uint16_t entries_used = 0;
+	uint64_t left_sibling, right_sibling;
+
+	entries_used = getBytesAsNumber(tree_pointer + 6, 2);
+
+	//assuming keys do not contain pertinent information
+	left_sibling = getBytesAsNumber(tree_pointer + 8, s_block.size_of_offsets);
+	if (left_sibling != UNDEF_ADDR)
+	{
+		pair.tree_address = left_sibling + s_block.base_address;
+		pair.heap_address = queue.pairs[queue.front].heap_address;
+		enqueuePair(pair);
+	}
+
+	right_sibling = getBytesAsNumber(tree_pointer + 8 + s_block.size_of_offsets, s_block.size_of_offsets);
+	if (right_sibling != UNDEF_ADDR)
+	{
+		pair.tree_address = right_sibling + s_block.base_address;
+		pair.heap_address = queue.pairs[queue.front].heap_address;
+		enqueuePair(pair);
+	}
+
+	for (int i = 0; i < entries_used; i++)
+	{
+		pair.tree_address = getBytesAsNumber(tree_pointer + 2*s_block.size_of_offsets + 16*(i + 1), s_block.size_of_offsets) + s_block.base_address;
+		pair.heap_address = queue.pairs[queue.front].heap_address;
+		enqueuePair(pair);
+	}
+
+}
+uint64_t readSnod(char* snod_pointer, char* heap_pointer, char* var_name)
+{
+	uint16_t num_symbols = getBytesAsNumber(snod_pointer + 6, 2);
+	Object* objects = (Object *)malloc(sizeof(Object)*num_symbols);
+	uint32_t cache_type;
+	Addr_Pair pair;
+	uint64_t ret = UNDEF_ADDR;
+
+	//get to entries
+	snod_pointer += 8;
+
+	for (int i = 0; i < num_symbols; i++)
+	{
+		objects[i].name_offset = getBytesAsNumber(snod_pointer + SYM_TABLE_ENTRY_SIZE*i, s_block.size_of_offsets);
+		objects[i].obj_header_address = getBytesAsNumber(snod_pointer + SYM_TABLE_ENTRY_SIZE*i + s_block.size_of_offsets, s_block.size_of_offsets) + s_block.base_address;
+
+		cache_type = getBytesAsNumber(snod_pointer + 2*s_block.size_of_offsets, 4);
+
+		//check if we have found the object we're looking for
+		if(strcmp(var_name, heap_pointer + 8 + 2*s_block.size_of_lengths + s_block.size_of_offsets + objects[i].name_offset) == 0)
+		{
+			ret = objects[i].obj_header_address;
+
+			//if another tree exists for this object, put it on the queue
+			if (cache_type == 1)
+			{
+				pair.tree_address = getBytesAsNumber(snod_pointer + 2*s_block.size_of_offsets + 8, s_block.size_of_offsets) + s_block.base_address;
+				pair.heap_address = getBytesAsNumber(snod_pointer + 3*s_block.size_of_offsets + 8, s_block.size_of_offsets) + s_block.base_address;
+				enqueuePair(pair);
+			}
+			break;
+		}
+	}
+
+	free(objects);
+	return ret;
 }
