9fb7c9a3aea9373f4767f021bd992b45362da633
Gene Harvey <gharveymn@gmail.com>
2017-09-05 13:26:11 -0500

    massively improves speed on loading doubles through a direct memcpy method

--- v7.3Files/src/mapping.c
+++ v7.3Files/src/mapping.c
@@ -297,7 +297,7 @@ void allocateSpace(Data* object)
 	}
 }
 
-void placeData(Data* object, char* data_pointer, uint64_t starting_index, uint64_t condition, size_t elem_size, ByteOrder byte_order)
+void placeData(Data* object, char* data_pointer, uint64_t starting_index, uint64_t condition, size_t elem_size, ByteOrder data_byte_order)
 {
 	int object_data_index = 0;
 	switch(object->type)
@@ -305,80 +305,96 @@ void placeData(Data* object, char* data_pointer, uint64_t starting_index, uint64
 		case INT8:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.i8_data[j] = (int8_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.i8_data[j] = (int8_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case UINT8:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.ui8_data[j] = (char)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.ui8_data[j] = (char)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case INT16:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.i16_data[j] = (int16_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.i16_data[j] = (int16_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case UINT16:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.ui16_data[j] = (uint16_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.ui16_data[j] = (uint16_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case INT32:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.i32_data[j] = (int32_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.i32_data[j] = (int32_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case UINT32:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.ui32_data[j] = (uint32_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.ui32_data[j] = (uint32_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case INT64:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.i64_data[j] = (int64_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.i64_data[j] = (int64_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case UINT64:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.ui64_data[j] = (uint64_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order);
+				object->data_arrays.ui64_data[j] = (uint64_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order);
 				object_data_index++;
 			}
 			break;
 		case SINGLE:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
-				object->data_arrays.single_data[j] = convertHexToSingle((uint32_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order));
+				object->data_arrays.single_data[j] = convertHexToSingle((uint32_t)getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order));
 				object_data_index++;
 			}
 			break;
 		case DOUBLE:
 			
-			//there is probably a way better way of doing this
-			for(uint64_t j = starting_index; j < condition; j++)
+			if(__BYTE_ORDER != data_byte_order)
+				for (uint64_t j = starting_index; j < condition; j++)
+				{
+					reverseBytes(data_pointer + object_data_index * elem_size, elem_size);
+					memcpy(&object->data_arrays.double_data[j], data_pointer + object_data_index * elem_size, elem_size);
+					object_data_index++;
+				}
+			else
 			{
-				object->data_arrays.double_data[j] = convertHexToDouble(getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order));
-				object_data_index++;
+				for (uint64_t j = starting_index; j < condition; j++)
+				{
+					memcpy(&object->data_arrays.double_data[j], data_pointer + object_data_index * elem_size, elem_size);
+					object_data_index++;
+				}
 			}
+
+			//there is probably a way better way of doing this
+			//for(uint64_t j = starting_index; j < condition; j++)
+			//{
+			//	object->data_arrays.double_data[j] = convertHexToDouble(getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order));
+			//	object_data_index++;
+			//}
 			break;
 		case REF:
 			for(uint64_t j = starting_index; j < condition; j++)
 			{
 				//these are addresses so we have to add the offset
-				object->data_arrays.udouble_data[j] = getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, byte_order) + s_block.base_address;
+				object->data_arrays.udouble_data[j] = getBytesAsNumber(data_pointer + object_data_index * elem_size, elem_size, data_byte_order) + s_block.base_address;
 				object_data_index++;
 			}
 			break;
